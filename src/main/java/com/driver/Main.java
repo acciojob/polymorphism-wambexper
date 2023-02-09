d7ff95b762afb3e3f1506399c56b841af4cfe088
wambexpeter <wambexpeter@gmail.com>
2023-02-09 15:53:13 +0300

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
 
-}
+
+        public static int product(int x, int y) {
+        return x*y;
+        }
+        public static int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public static double product(double x, double y){
+
+            return x*y;
+        }
+        }
+        public static void main(int[] args){
+            Product p = new Product();
+           int ans = p.product(2,4);
+           int ans2 = p.product(5,6,7);
+            double ans3 = p.product(1.2,2.5);
+            System.out.println(ans + " " + ans2 + " " + ans3);
+        }
+    }
+
